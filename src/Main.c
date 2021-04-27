379d10748a2d35a5ab58c88c5c0e7be67d29f283
Blake Dukes <Blakematthewdukes@gmail.com>
2021-04-26 22:20:29 -0400

    setup libcurl connection to wordsapi

--- src/Main.c
+++ src/Main.c
@@ -1,59 +1,68 @@
-/* Include gtk
-#include <gtk/gtk.h>
-
-static void on_activate (GtkApplication *app) {
-  // Create a new window
-  GtkWidget *window = gtk_application_window_new (app);
-  // Create a new button
-  GtkWidget *button = gtk_button_new_with_label ("Hello, World!");
-  // When the button is clicked, close the window passed as an argument
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_close), window);
-  gtk_window_set_child (GTK_WINDOW (window), button);
-  gtk_window_present (GTK_WINDOW (window));
-}
-
-int main (int argc, char *argv[]) {
-  // Create a new application
-  GtkApplication *app = gtk_application_new ("com.example.GtkApplication",
-                                             G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL);
-  return g_application_run (G_APPLICATION (app), argc, argv);
-} */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <curl/curl.h> /* has to be installed with "sudo apt-get install libcurl4-openssl-dev" */
+
+
+/*Important notes:
+use of the api requires a subscription to the WordsAPI page. https://rapidapi.com/dpventures/api/wordsapi/endpoints
+The api key listed below is the personal api key of Blake Dukes, charges are incurred after 2500 api calls in a given day. 
+Please do not misuse the api in a way that would incurr charges to the account. Be extremely careful with looping etc.
+*/
 
 int main()
 {
-    /*define variables for user input*/
-    char menu_option;    
 
-    do
+  void getRequest();
+
+  /*define variables for user input*/
+  char menu_option;
+
+  do
+  {
+    /*print out the main menu, taking in user input to select a function*/
+    printf("Main Menu\n");
+    printf("0: Haiku Verification\n");
+    printf("1: Couplet Verification\n");
+    printf("e: Exit Program\n");
+    printf(" Please enter an option from the main menu: ");
+    scanf(" %c", &menu_option);
+
+    switch (menu_option)
     {
-        /*print out the main menu, taking in user input to select a function*/
-        printf("Main Menu\n");
-        printf("0: Haiku Verification\n");
-        printf("1: Couplet Verification\n");
-        printf("e: Exit Program\n");
-        printf(" Please enter an option from the main menu: ");
-        scanf(" %c", &menu_option);
+    case '0':
+      getRequest();
+      break;
+    case '1':
 
-        switch (menu_option)
-        {
-        case '0':
-            
-            break;
-        case '1':
-            
-            break;
-        case 'e':
-            /*stop program execution*/
-            break;
-        default:
-            /*handle invalid menu choices*/
-            printf("invalid input");
-            break;
-        }
+      break;
+    case 'e':
+      /*stop program execution*/
+      break;
+    default:
+      /*handle invalid menu choices*/
+      printf("invalid input");
+      break;
+    }
 
-    } while (menu_option != 'e');
+  } while (menu_option != 'e');
+  
+  curl_global_cleanup();
+
+  return 0;
+}
+
+void getRequest()
+{
+  CURL *hnd = curl_easy_init();
+
+  curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
+  curl_easy_setopt(hnd, CURLOPT_URL, "https://wordsapiv1.p.rapidapi.com/words/.22-caliber/pertainsTo");
+
+  struct curl_slist *headers = NULL;
+  headers = curl_slist_append(headers, "x-rapidapi-key: c6ccdbfbbbmshe7408fbb59b5b09p1c4661jsn1ef62d57e9b0");
+  headers = curl_slist_append(headers, "x-rapidapi-host: wordsapiv1.p.rapidapi.com");
+  curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+  CURLcode ret = curl_easy_perform(hnd);
+}
